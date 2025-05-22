68e3d1bc9271e89f3224306706acb5f254d701f4
LuxBug <75321221+luxbg@users.noreply.github.com>
2025-05-22 08:56:39 +0300

    change name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "AnyCodable",
+    name: "AnyCodableSui",
     platforms:  [
         .iOS(.v9),
         .macOS(.v10_10),
@@ -14,8 +14,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "AnyCodable",
-            targets: ["AnyCodable"]
+            name: "AnyCodableSui",
+            targets: ["AnyCodableSui"]
         ),
     ],
     dependencies: [
@@ -26,12 +26,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "AnyCodable",
+            name: "AnyCodableSui",
             dependencies: []
         ),
         .testTarget(
             name: "AnyCodableTests",
-            dependencies: ["AnyCodable"]
+            dependencies: ["AnyCodableSui"]
         ),
     ]
 )
